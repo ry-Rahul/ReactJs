02ae94c31872ebb554faf1ea2336e31620758d29
Rahul Yadav <ryrahul12345@gmail.com>
2023-10-09 20:54:53 +0530

    updated

---
+++ MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,40 @@
+import conf from "../conf/conf.js";
+import { Client, Account, Id } from "appwrite";
+
+export class AuthService {
+    Client = new Client();
+    account;
+
+    constructor() {
+        this.Client.setEndpoint(conf.appwriteUrl).setProject(
+            conf.appwriteProjectId
+        );
+
+        this.account = new Account(this.Client);
+    }
+    f
+    // Login with email and password and return user account
+    async createAccount({ email, password, name }) {
+        try {
+            // Create user account
+            const userAccount = await this.account.create(
+                Id.unique(),
+                email,
+                password,
+                name
+            ); // Create user account
+            if (userAccount) {
+              // Call another function 
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            console.log(error);
+            throw error;
+        }
+    }
+}
+
+const authService = new AuthService();
+
+export default AuthService;
